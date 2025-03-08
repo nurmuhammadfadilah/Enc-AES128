83b73eeb809387c6548134e2c47a00e4ba466976
nurmuhammadfadilah <muhammfadilah7@gmail.com>
2025-03-08 17:31:38 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,11 @@ import Decrypt from "../components/Decrypt";
 
 export default function App() {
   const [activePage, setActivePage] = useState("encrypt");
-  const [isMounted, setIsMounted] = useState(false);
   
   // Mengubah title halaman saat aktifPage berubah
   useEffect(() => {
-    setIsMounted(true);
     document.title = activePage === "encrypt" ? "Encrypt File - AES128" : "Decrypt File - AES128";
   }, [activePage]);
-  
-  if (!isMounted) return null;
 
   return (
     <div className="min-h-screen bg-gray-100">
